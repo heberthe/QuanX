b32efe2daf5624b07e34de879c087d8255040356
Hebert.He <lengfenghhx@hotmail.com>
2023-04-28 14:20:47 +0800

    feat: add jinkela signin

--- SignIn.js
+++ SignIn.js
@@ -45,7 +45,7 @@ function checkin() {
 			'Cookie': JSON.parse(cookie) //用户鉴权Cookie
 		}
 	}
-	console.log(`\n cookie: `, JSON.parse(cookie));
+	console.log(`\n cookie: `, $.read('hex_signheader_jinkela'));
 	return new Promise((resolve) => { //主函数返回Promise实例对象, 以便后续调用时可以实现顺序执行异步函数
 		$.post(pointUrl, (error, resp, data) => { //使用post请求查询, 再使用回调函数处理返回的结果
 			try { //使用try方法捕获可能出现的代码异常
